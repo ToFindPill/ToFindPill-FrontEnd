6c034d10406e97ac2806815bdfe14ff4f6bbdd03
nahyun27 <ksknh7@naver.com>
2024-05-12 23:40:52 +0900

    [docs] [TFP-61] add README.md

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+// };
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+      },
+    ],
+    'babel-plugin-styled-components',
+    'react-native-reanimated/plugin',
+  ],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
 };
